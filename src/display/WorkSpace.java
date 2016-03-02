8695bc0cde5b3274785cab2a384087175df89c1c
mykuryshev <michael.kuryshev@duke.edu>
2016-03-01 23:42:05 -0500

    Fixed image selecting, layout of scene, pen drawing

--- src/display/WorkSpace.java
+++ src/display/WorkSpace.java
@@ -184,9 +184,9 @@ public class WorkSpace extends Screen {
 		turtleView = new TurtleView(thing, getRoot(),
 				layer2.getGraphicsContext2D(), Color.BLACK);
 		turtleView.getImage().setX(
-				turtleModel.getPositionX() - thing.getFitWidth() / 2);
+				turtleModel.getPositionX() + UIConstants.CANVAS_SIZE/2);
 		turtleView.getImage().setY(
-				turtleModel.getPositionY() - thing.getFitHeight() / 2);
+				turtleModel.getPositionY() + UIConstants.CANVAS_SIZE/2);
 		turtleModel.addObserver(turtleView);
 		turtleModel.notifyObservers();
 
@@ -205,6 +205,7 @@ public class WorkSpace extends Screen {
 				layer2.getGraphicsContext2D(), Color.BLACK);
 		turtleModel.addObserver(turtleView);
 		turtleModel.notifyObservers();
+		turtleModel.penDown();
 
 		modelMap = new ModelMap();
 		modelMap.setTurtle(turtleModel);
@@ -215,8 +216,8 @@ public class WorkSpace extends Screen {
 	private void setInputPane() {
 		HBox commandLine = new HBox();
 		inputText = new TextArea();
-		inputText.setMinWidth(450);
-		inputText.setMinHeight(200);
+		inputText.setMinSize(450, 200);
+		inputText.setMaxSize(450, 200);
 		commandLine.getChildren().add(inputText);
 		Button inputButton = featureMaker.makeB("Go",
 				event -> readInput(parser, inputText));
@@ -228,10 +229,8 @@ public class WorkSpace extends Screen {
 
 	private void setHistoryPane() {
 		SPane history = new SPane(900, 25);
-		history.myPane.setMinWidth(360);
-		history.myPane.setMinHeight(475);
-		//history.myPane.setMaxSize(UIConstants.CANVAS_SIZE,
-		//		UIConstants.CANVAS_SIZE - UIConstants.BORDER_WIDTH);
+		history.myPane.setMinSize(360, 475);
+		history.myPane.setMaxSize(360, 475);
 		getRoot().getChildren().addAll(history.myRoot);
 		HistoryPaneModel hpm = new HistoryPaneModel();
 		hpv = new HistoryPaneView(history.myBox, inputText);
@@ -298,17 +297,6 @@ public class WorkSpace extends Screen {
 		parser.parseText(input.getText());
 		input.clear();
 	}
-
-	// private void readInput(CommandParser parser, TextArea input){
-	// //double output = parser.parseText(input.getText());
-	// try{
-	// parser.parseText(input.getText());
-	// input.clear();
-	// }
-	// catch(String msg){
-	// showError("u messed up");
-	// }
-	// }
 
 	protected void showError(String message) {
 		Alert alert = new Alert(AlertType.ERROR);
